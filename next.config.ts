e5c17f7a646efb379a9fcab1f6eef28be77b9f91
arif saputra <ArifYogiSaputra_1701620136@mhs.unj.ac.id>
2026-05-04 20:04:42 +0700

    chore: enable static export

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	// For local development, basePath is '/'
-	// This file will be overwritten during deployment with the appropriate basePath
-	images: {
-		remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-	},
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
